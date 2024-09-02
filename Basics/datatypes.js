0bd9c870a4ee25e3b3c12d11b6c420331dde8b6b
Hasnain Ahmad <146635268+hasnain-a7@users.noreply.github.com>
2024-09-02 20:37:55 +0000

    datatypes

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -1,12 +1,32 @@
-let name = "Hasnain"
-let age = 18
-let isLoggedIn = false
-let state;
-
+//                               Primitive
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 // number => 2 to power 53
+  let age = 18
 // bigint
+
 // string => ""
+let name = "Hasnain"
+
 // boolean => true/false
+let isLoggedIn = false
+
 // null => standalone value
+
 // undefined => 
+let state;
 // symbol => unique
+
+//                              Non Primitive 
+// Array, Objects, Functions
+
+const heros = ["Superman", "Batman", "Spiderman"];
+
+let myObj = {
+    name: "Hasnain",
+    age: 18,
+}
+
+function myfun(){
+    console.log("Hello World");
+    
+}
