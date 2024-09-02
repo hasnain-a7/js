d6090d9e38be2ccf4662c8fbd642fc997669ae5c
Hasnain Ahmad <146635268+hasnain-a7@users.noreply.github.com>
2024-09-02 20:51:06 +0000

    memory

---
+++ Basics/memory.js
@@ -0,0 +1,9 @@
+// stack and heap meomry
+
+// primitive data type are (Stack)
+// non-primitive data type are (Heap)
+
+// it means if you change value of stack memoary's variable you can only change
+// value of is copy
+// if you change the value of heap memoary's variable so you can change original value
+// of it not it's copy
