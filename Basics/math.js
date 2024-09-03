5af053de4e80b8fc1674626edb5c577fe3287a82
Hasnain Ahmad <146635268+hasnain-a7@users.noreply.github.com>
2024-09-03 20:01:08 +0000

    math

---
+++ Basics/math.js
@@ -0,0 +1,11 @@
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+// /*
+// to get random value use formula 
+// // const min = 10
+// // const max = 20
+
+// // console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
+// */
+
